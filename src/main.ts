a394e84abebb818e8ec40b14469f0a966b7367bc
Miller Arias <millerjavierarias@gmail.com>
2024-05-16 19:35:02 -0500

    Feat:footer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import "./style.sass";
-import { createHeader, createMain } from "./UIelements";
+import { createFooter, createHeader, createMain } from "./UIelements";
 
 const app = document.querySelector("#app");
 
 const header = createHeader();
 const main = createMain();
+const footer = createFooter();
 
-app?.append(header, main);
+app?.append(header, main, footer);
